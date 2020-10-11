99b786d03cf8dbcfba08a42ecdd3b06ca2d39308
Paul Jezek <Paul.Jezek@updatepoint.de>
2020-10-11 22:03:38 +0200

    Added Projectfiles

---
+++ endpointmanager.networkusage/Connectivity.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.Connectivity;
+
+namespace endpointmanager.connectivity
+{
+    class WinRTConnectivity
+    {
+
+        private static async Task<IReadOnlyList<Windows.Networking.Connectivity.AttributedNetworkUsage>> GetAttributedNetworkUsageAsync(ConnectionProfile connectionProfile, DateTime startTime, DateTime endTime, NetworkUsageStates networkUsageStates)
+        {
+            try
+            {
+                DateTimeOffset StartTimeOffset = new DateTimeOffset(startTime);
+                DateTimeOffset EndTimeOffset = new DateTimeOffset(endTime);
+                return await connectionProfile.GetAttributedNetworkUsageAsync(StartTimeOffset, EndTimeOffset, networkUsageStates);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static async Task<IReadOnlyList<AttributedNetworkUsage>> GetAttributedNetworkUsageSync(ConnectionProfile connectionProfile, DateTime startTime, DateTime endTime, NetworkUsageStates networkUsageStates)
+        {
+            return await GetAttributedNetworkUsageAsync(connectionProfile, startTime, endTime, networkUsageStates);
+        }
+
+        public static IReadOnlyList<AttributedNetworkUsage> GetAttributedNetworkUsage(ConnectionProfile connectionProfile, DateTime startTime, DateTime endTime, NetworkUsageStates networkUsageStates)
+        {
+            Task<IReadOnlyList<AttributedNetworkUsage>> task = Task.Run(() => GetAttributedNetworkUsageSync(connectionProfile, startTime, endTime, networkUsageStates));
+            return task.Result;
+        }
+
+
+        private static async Task<IReadOnlyList<ProviderNetworkUsage>> GetProviderNetworkUsageAsync(ConnectionProfile connectionProfile, DateTime startTime, DateTime endTime, NetworkUsageStates networkUsageStates)
+        {
+            try
+            {
+                DateTimeOffset StartTimeOffset = new DateTimeOffset(startTime);
+                DateTimeOffset EndTimeOffset = new DateTimeOffset(endTime);
+
+                return await connectionProfile.GetProviderNetworkUsageAsync(StartTimeOffset, EndTimeOffset, networkUsageStates);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static async Task<IReadOnlyList<NetworkUsage>> GetNetworkUsageAsync(ConnectionProfile connectionProfile, DateTime startTime, DateTime endTime, DataUsageGranularity granularity, NetworkUsageStates networkUsageStates)
+        {
+            try
+            {
+                DateTimeOffset StartTimeOffset = new DateTimeOffset(startTime);
+                DateTimeOffset EndTimeOffset = new DateTimeOffset(endTime);
+               
+                return await connectionProfile.GetNetworkUsageAsync(StartTimeOffset, EndTimeOffset, granularity, networkUsageStates);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static async Task<IReadOnlyList<ConnectivityInterval>> GetConnectivityIntervalsAsync(ConnectionProfile connectionProfile, DateTime startTime, DateTime endTime, NetworkUsageStates networkUsageStates)
+        {
+            try
+            {
+                DateTimeOffset StartTimeOffset = new DateTimeOffset(startTime);
+                DateTimeOffset EndTimeOffset = new DateTimeOffset(endTime);
+
+                return await connectionProfile.GetConnectivityIntervalsAsync(StartTimeOffset, EndTimeOffset, networkUsageStates);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static async Task<IReadOnlyList<NetworkUsage>> GetNetworkUsageSync(ConnectionProfile connectionProfile, DateTime startTime, DateTime endTime, DataUsageGranularity granularity, NetworkUsageStates networkUsageStates)
+        {
+            return await GetNetworkUsageAsync(connectionProfile, startTime, endTime, granularity, networkUsageStates);
+        }
+
+        static async Task<IReadOnlyList<ConnectivityInterval>> GetConnectivityIntervalsSync(ConnectionProfile connectionProfile, DateTime startTime, DateTime endTime, NetworkUsageStates networkUsageStates)
+        {
+            return await GetConnectivityIntervalsAsync(connectionProfile, startTime, endTime, networkUsageStates);
+        }
+
+        public static async Task<IReadOnlyList<ProviderNetworkUsage>> GetProviderNetworkUsageSync(ConnectionProfile connectionProfile, DateTime startTime, DateTime endTime, NetworkUsageStates networkUsageStates)
+        {
+            return await GetProviderNetworkUsageAsync(connectionProfile, startTime, endTime, networkUsageStates);
+        }
+
+        public static IReadOnlyList<ProviderNetworkUsage> GetProviderNetworkUsage(ConnectionProfile connectionProfile, DateTime startTime, DateTime endTime, NetworkUsageStates networkUsageStates)
+        {
+            Task<IReadOnlyList<ProviderNetworkUsage>> task = Task.Run(() => GetProviderNetworkUsageSync(connectionProfile, startTime, endTime, networkUsageStates));
+            return task.Result;
+        }
+
+        public static IReadOnlyList<NetworkUsage> GetNetworkUsage(ConnectionProfile connectionProfile, DateTime startTime, DateTime endTime, DataUsageGranularity granularity, NetworkUsageStates networkUsageStates)
+        {
+            Task<IReadOnlyList<NetworkUsage>> task = Task.Run(() => GetNetworkUsageSync(connectionProfile, startTime, endTime, granularity, networkUsageStates));
+            return task.Result;
+        }
+
+        public static IReadOnlyList<ConnectivityInterval> GetConnectivityIntervals(ConnectionProfile connectionProfile, DateTime startTime, DateTime endTime, NetworkUsageStates networkUsageStates)
+        {
+            Task<IReadOnlyList<ConnectivityInterval>> task = Task.Run(() => GetConnectivityIntervalsSync(connectionProfile, startTime, endTime, networkUsageStates));
+            return task.Result;
+        }
+
+        public static IReadOnlyList<Windows.Networking.Connectivity.ConnectionProfile> GetConnectionProfiles()
+        {
+            return NetworkInformation.GetConnectionProfiles();
+        }
+
+        public static Windows.Networking.Connectivity.ConnectionProfile GetInternetConnectionProfile()
+        {
+            return NetworkInformation.GetInternetConnectionProfile();
+        }
+    }
+}
---
+++ endpointmanager.networkusage/NetworkUsage.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using Windows.Networking.Connectivity;
+using endpointmanager.connectivity;
+
+namespace endpointmanager.networkusage
+{
+    [Cmdlet("Get", "ConnectionProfiles")]
+    [OutputType(typeof(Windows.Networking.Connectivity.ConnectionProfile))]
+    public class GetConnectionProfilesCommand : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            foreach (Windows.Networking.Connectivity.ConnectionProfile profiles in WinRTConnectivity.GetConnectionProfiles())
+            {
+                WriteObject(profiles);
+            }
+        }
+    }
+
+    [Cmdlet("Get", "ConnectionCost")]
+    [OutputType(typeof(Windows.Networking.Connectivity.ConnectionCost))]
+    public class GetConnectionCostCommand : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Windows.Networking.Connectivity.ConnectionProfile ConnectionProfile
+        {
+            get { return connectionProfile; }
+            set { connectionProfile = value; }
+        }
+        private Windows.Networking.Connectivity.ConnectionProfile connectionProfile;
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(connectionProfile.GetConnectionCost());
+        }
+    }
+
+    [Cmdlet("Get", "DataPlanStatus")]
+    [OutputType(typeof(Windows.Networking.Connectivity.DataPlanStatus))]
+    public class GetDataPlanStatusCommand : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Windows.Networking.Connectivity.ConnectionProfile ConnectionProfile
+        {
+            get { return connectionProfile; }
+            set { connectionProfile = value; }
+        }
+        private Windows.Networking.Connectivity.ConnectionProfile connectionProfile;
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(connectionProfile.GetDataPlanStatus());
+        }
+    }
+
+    [Cmdlet("Get", "NetworkNames")]
+    [OutputType(typeof(string))]
+    public class GetNetworkNamesCommand : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Windows.Networking.Connectivity.ConnectionProfile ConnectionProfile
+        {
+            get { return connectionProfile; }
+            set { connectionProfile = value; }
+        }
+        private Windows.Networking.Connectivity.ConnectionProfile connectionProfile;
+
+        protected override void ProcessRecord()
+        {
+            foreach (string networkName in connectionProfile.GetNetworkNames())
+            {
+                WriteObject(networkName);
+            }
+        }
+    }
+
+    [Cmdlet("Get", "SignalBars")]
+    [OutputType(typeof(System.Nullable<byte>))]
+    public class GetSignalBarsCommand : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Windows.Networking.Connectivity.ConnectionProfile ConnectionProfile
+        {
+            get { return connectionProfile; }
+            set { connectionProfile = value; }
+        }
+        private Windows.Networking.Connectivity.ConnectionProfile connectionProfile;
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(connectionProfile.GetSignalBars());
+        }
+    }
+
+    [Cmdlet("Get", "DomainConnectivityLevel")]
+    [OutputType(typeof(DomainConnectivityLevel))]
+    public class GetDomainConnectivityLevelCommand : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Windows.Networking.Connectivity.ConnectionProfile ConnectionProfile
+        {
+            get { return connectionProfile; }
+            set { connectionProfile = value; }
+        }
+        private Windows.Networking.Connectivity.ConnectionProfile connectionProfile;
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(connectionProfile.GetDomainConnectivityLevel());
+        }
+    }
+
+
+    [Cmdlet("Get", "NetworkConnectivityLevel")]
+    [OutputType(typeof(NetworkConnectivityLevel))]
+    public class GetNetworkConnectivityLevelCommand : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Windows.Networking.Connectivity.ConnectionProfile ConnectionProfile
+        {
+            get { return connectionProfile; }
+            set { connectionProfile = value; }
+        }
+        private Windows.Networking.Connectivity.ConnectionProfile connectionProfile;
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(connectionProfile.GetNetworkConnectivityLevel());
+        }
+    }
+
+    [Cmdlet("Get", "InternetConnectionProfile")]
+    [OutputType(typeof(Windows.Networking.Connectivity.ConnectionProfile))]
+    public class GetInternetConnectionProfileCommand : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            WriteObject(WinRTConnectivity.GetInternetConnectionProfile());
+        }
+    }
+
+    [Cmdlet("Get", "AttributedNetworkUsage")]
+    [OutputType(typeof(Windows.Networking.Connectivity.AttributedNetworkUsage))]
+    public class GetAttributedNetworkUsageCommand : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Windows.Networking.Connectivity.ConnectionProfile ConnectionProfile
+        {
+            get { return connectionProfile; }
+            set { connectionProfile = value; }
+        }
+        private Windows.Networking.Connectivity.ConnectionProfile connectionProfile;
+
+        [Parameter(Mandatory = false)]
+        public DateTime StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+        private DateTime startTime = DateTime.Now.AddDays(-60);
+
+        [Parameter(Mandatory = false)]
+        public DateTime EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+        private DateTime endTime = DateTime.Now;
+
+        [Parameter(Mandatory = false)]
+        public NetworkUsageStates NetworkUsageStates
+        {
+            get { return networkUsageStates; }
+            set { networkUsageStates = value; }
+        }
+        private NetworkUsageStates networkUsageStates = new NetworkUsageStates { Roaming = TriStates.DoNotCare, Shared = TriStates.DoNotCare };
+
+
+        protected override void ProcessRecord()
+        {
+            IReadOnlyList<AttributedNetworkUsage> networkUsageList = WinRTConnectivity.GetAttributedNetworkUsage(connectionProfile, startTime, endTime, networkUsageStates);
+            if (networkUsageList.Count > 0)
+            {
+                foreach (AttributedNetworkUsage networkUsage in networkUsageList)
+                {
+                    WriteObject(networkUsage);
+                }
+            }
+        }
+    }
+    
+    [Cmdlet("Get", "NetworkUsage")]
+    [OutputType(typeof(Windows.Networking.Connectivity.NetworkUsage))]
+    public class GetNetworkUsageCommand : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Windows.Networking.Connectivity.ConnectionProfile ConnectionProfile
+        {
+            get { return connectionProfile; }
+            set { connectionProfile = value; }
+        }
+        private Windows.Networking.Connectivity.ConnectionProfile connectionProfile;
+
+        [Parameter(Mandatory = false)]
+        public Windows.Networking.Connectivity.DataUsageGranularity Granularity
+        {
+            get { return granularity; }
+            set { granularity = value; }
+        }
+        private Windows.Networking.Connectivity.DataUsageGranularity granularity = DataUsageGranularity.Total;
+
+        [Parameter(Mandatory = false)]
+        public DateTime StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+        private DateTime startTime = DateTime.Now.AddDays(-60);
+
+        [Parameter(Mandatory = false)]
+        public DateTime EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+        private DateTime endTime = DateTime.Now;
+
+        [Parameter(Mandatory = false)]
+        public NetworkUsageStates NetworkUsageStates
+        {
+            get { return networkUsageStates; }
+            set { networkUsageStates = value; }
+        }
+        private NetworkUsageStates networkUsageStates = new NetworkUsageStates { Roaming = TriStates.DoNotCare, Shared = TriStates.DoNotCare };
+
+        protected override void ProcessRecord()
+        {
+            IReadOnlyList<NetworkUsage> networkUsageList = WinRTConnectivity.GetNetworkUsage(ConnectionProfile, startTime, endTime, granularity, networkUsageStates);
+            if (networkUsageList.Count > 0)
+            {
+                foreach (NetworkUsage networkUsage in networkUsageList)
+                {
+                    WriteObject(networkUsage);
+                }
+            }
+        }
+    }
+
+    [Cmdlet("Get", "ConnectivityIntervals")]
+    [OutputType(typeof(Windows.Networking.Connectivity.ConnectivityInterval))]
+    public class GetConnectivityIntervalsCommand : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Windows.Networking.Connectivity.ConnectionProfile ConnectionProfile
+        {
+            get { return connectionProfile; }
+            set { connectionProfile = value; }
+        }
+        private Windows.Networking.Connectivity.ConnectionProfile connectionProfile;
+
+        [Parameter(Mandatory = false)]
+        public DateTime StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+        private DateTime startTime = DateTime.Now.AddDays(-60);
+
+        [Parameter(Mandatory = false)]
+        public DateTime EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+        private DateTime endTime = DateTime.Now;
+
+        [Parameter(Mandatory = false)]
+        public NetworkUsageStates NetworkUsageStates
+        {
+            get { return networkUsageStates; }
+            set { networkUsageStates = value; }
+        }
+        private NetworkUsageStates networkUsageStates = new NetworkUsageStates { Roaming = TriStates.DoNotCare, Shared = TriStates.DoNotCare };
+
+        protected override void ProcessRecord()
+        {
+            IReadOnlyList<ConnectivityInterval> connectivityIntervalList = WinRTConnectivity.GetConnectivityIntervals(ConnectionProfile, startTime, endTime, networkUsageStates);
+            if (connectivityIntervalList.Count > 0)
+            {
+                foreach (ConnectivityInterval connectivityInterval in connectivityIntervalList)
+                {
+                    WriteObject(connectivityInterval);
+                }
+            }
+        }
+    }
+
+    [Cmdlet("Get", "ProviderNetworkUsage")]
+    [OutputType(typeof(Windows.Networking.Connectivity.ConnectivityInterval))]
+    public class GetProviderNetworkUsageCommand : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Windows.Networking.Connectivity.ConnectionProfile ConnectionProfile
+        {
+            get { return connectionProfile; }
+            set { connectionProfile = value; }
+        }
+        private Windows.Networking.Connectivity.ConnectionProfile connectionProfile;
+
+        [Parameter(Mandatory = false)]
+        public DateTime StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+        private DateTime startTime = DateTime.Now.AddDays(-60);
+
+        [Parameter(Mandatory = false)]
+        public DateTime EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+        private DateTime endTime = DateTime.Now;
+
+        [Parameter(Mandatory = false)]
+        public NetworkUsageStates NetworkUsageStates
+        {
+            get { return networkUsageStates; }
+            set { networkUsageStates = value; }
+        }
+        private NetworkUsageStates networkUsageStates = new NetworkUsageStates { Roaming = TriStates.DoNotCare, Shared = TriStates.DoNotCare };
+
+        protected override void ProcessRecord()
+        {
+            IReadOnlyList<ProviderNetworkUsage> networkUsageList = WinRTConnectivity.GetProviderNetworkUsage(ConnectionProfile, startTime, endTime, networkUsageStates);
+            if (networkUsageList.Count > 0)
+            {
+                foreach (ProviderNetworkUsage networkUsage in networkUsageList)
+                {
+                    WriteObject(networkUsage);
+                }
+            }
+        }
+    }
+
+    [Cmdlet("New", "NetworkUsageStates")] 
+    [OutputType(typeof(NetworkUsageStates))]
+    public class GetNetworkUsageStatesCommand : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Windows.Networking.Connectivity.TriStates Roaming
+        {
+            get { return roaming; }
+            set { roaming = value; }
+        }
+        private Windows.Networking.Connectivity.TriStates roaming;
+
+        [Parameter(Mandatory = true)]
+        public Windows.Networking.Connectivity.TriStates Shared
+        {
+            get { return shared; }
+            set { shared = value; }
+        }
+        private Windows.Networking.Connectivity.TriStates shared;
+
+        protected override void ProcessRecord()
+        {
+            NetworkUsageStates result = new NetworkUsageStates { Roaming = roaming, Shared = shared };
+            WriteObject(result);
+        }
+    }
+}
